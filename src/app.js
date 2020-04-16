8e86f678e8767c84e301d934b9fdba04909556e5
Scott Strool <scott.strool@snyk.io>
2020-04-16 18:11:00 -0400

    prepare for heroku deploy

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for Express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -102,6 +103,6 @@ app.get('/weather', (req, res) => {
     })
 
 //Start the Server
-app.listen(3000,()=>{
-    console.log('Server is up')
+app.listen(port, ()=>{
+    console.log('Server is up on port '+port)
 })
